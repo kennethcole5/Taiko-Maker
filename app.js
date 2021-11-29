c7a67da97ae2dc31dd357aeb6670ba63d2980fad
Kennethcole5 <kenneth.cole@selu.edu>
2021-11-28 19:49:06 -0600

    Did css on slectors, added constructor for drumkit in app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+class DrumKit {
+    constructor() {
+        this.pads = document.querySelectorAll("");
+        this.kickAduio = document.querySelector(".kick-sound");
+        this.snareAduio = document.querySelector(".snare-sound");
+        this.hihatAduio = document.querySelector(".hihat-sound");
+        this.index = 0;
+    }
+}
