a69b0aaff4f9e118bbdb5e38122d2faf9b10b320
Kennethcole5 <kenneth.cole@selu.edu>
2021-12-08 23:17:34 -0600

    Added second event listener to drumKit object so the animation's keep going

--- app.js
+++ app.js
@@ -33,6 +33,9 @@ const drumKit = new DrumKit();
 
 drumKit.pads.forEach(pad => {
     pad.addEventListener("click", drumKit.activePad);
+    pad.addEventListener('animationend', function () {
+        this.style.animation = "";
+    });
 });
 
 
