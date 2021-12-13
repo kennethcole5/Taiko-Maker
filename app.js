39e098dabf6164681e4c5a5d0b0445ad59bb524d
Kennethcole5 <kenneth.cole@selu.edu>
2021-12-13 17:01:04 -0600

    Added reset for current time, kick and snare are alright functionality, hihats aren't really coming through. Put some breaks in play() think you know what it is

--- app.js
+++ app.js
@@ -21,12 +21,15 @@ class DrumKit {
             if (bar.classList.contains("active")) {
                 //Check each sound
                 if (bar.classList.contains("kick-pad")) {
+                    this.kickAudio.currentTIme = 0;
                     this.kickAudio.play();
                 }
                 if (bar.classList.contains("snare-pad")) {
+                    this.snareAudio.currentTIme = 0;
                     this.snareAudio.play();
                 }
                 if (bar.classList.contains("hihat-pad")) {
+                    this.hihatAudio.currentTIme = 0;
                     this.kickAudio.play();
                 }
             }
